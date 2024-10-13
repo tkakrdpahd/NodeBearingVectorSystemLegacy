0856330d012a706f11ba1e27681bbe272804bb58
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-10-13 16:53:17 -0400

    Update NodeVector.h

--- module/NodeVector.h
+++ module/NodeVector.h
@@ -1,3 +1,13 @@
 /* NodeVector.h
- *
+ * Linked file NodeVector.cpp
+ * Security: Top Secret
+ * Author: Minseok Doo
+ * Date: Oct 13, 2024
+ * 
+ * Purpose of Class
+ * Declare Node Vector
+ * 
+ * Equations
+ * Equ(1): \vec{N_i}=\left(r_i,\theta_i,\phi_i\right)
  */
+
