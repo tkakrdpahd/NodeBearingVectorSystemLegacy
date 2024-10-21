2bb4bb80b1667c21cb46c58a674bb3c222cda9ac
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-10-21 11:28:10 -0400

    Update SocketServer.h

--- module/SocketServer.h
+++ module/SocketServer.h
@@ -9,3 +9,29 @@
  * 
  * Equations
  */
+#ifndef SOCKETSERVER_H
+#define SOCKETSERVER_H
+
+#include <string>
+#include <arpa/inet.h>
+
+// GetAttributesManagerData 함수 선언
+std::string GetAttributesManagerData();
+
+class SocketServer {
+public:
+    SocketServer(int serverPort);
+    ~SocketServer();
+
+    bool startServer();
+    void listenForClients();
+    void sendResponse(int clientSocket, const std::string& message);
+    void closeServer();
+
+private:
+    int serverPort;
+    int serverSocketFd;
+    struct sockaddr_in serverAddr;
+};
+
+#endif // SOCKETSERVER_H
