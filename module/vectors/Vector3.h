2441758689cbef68e502fcdedc36a3788e7933dd
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-10-24 10:49:23 -0400

    Update Vector3.h

--- module/vectors/Vector3.h
+++ module/vectors/Vector3.h
@@ -2,29 +2,46 @@
  * Security: Confidential
  * Author: Minseok Doo
  * Date: Oct 24, 2024
- * 
  */
-
-#include <vector>
 
 #ifndef VECTOR3_H
 #define VECTOR3_H
 
+#include <cmath>  // sqrt() 함수 사용을 위해 필요
+
 // Simple Vector3 struct for vector operations
 struct Vector3 {
     float x, y, z;
-    Vector3(float xVal = 0, float yVal = 0, float zVal = 0) : x(xVal), y(yVal), z(zVal) {}
+
+    // Constructor with default values
+    Vector3(float xVal = 0, float yVal = 0, float zVal = 0) 
+        : x(xVal), y(yVal), z(zVal) {}
 
     // Vector addition
-    Vector3 operator+(const Vector3& v) const { return Vector3(x + v.x, y + v.y, z + v.z); }
+    Vector3 operator+(const Vector3& v) const {
+        return Vector3(x + v.x, y + v.y, z + v.z);
+    }
+
     // Vector subtraction
-    Vector3 operator-(const Vector3& v) const { return Vector3(x - v.x, y - v.y, z - v.z); }
+    Vector3 operator-(const Vector3& v) const {
+        return Vector3(x - v.x, y - v.y, z - v.z);
+    }
+
     // Scalar multiplication (Vector3 * float)
-    Vector3 operator*(float scalar) const { return Vector3(x * scalar, y * scalar, z * scalar); }
+    Vector3 operator*(float scalar) const {
+        return Vector3(x * scalar, y * scalar, z * scalar);
+    }
+
     // Scalar division
-    Vector3 operator/(float scalar) const { return Vector3(x / scalar, y / scalar, z / scalar); }
+    Vector3 operator/(float scalar) const {
+        return Vector3(x / scalar, y / scalar, z / scalar);
+    }
+
     // Dot product
-    float dot(const Vector3& v) const { return x * v.x + y * v.y + z * v.z; }
+    float dot(const Vector3& v) const {
+        return x * v.x + y * v.y + z * v.z;
+    }
+
     // Cross product
     Vector3 cross(const Vector3& v) const {
         return Vector3(
@@ -33,8 +50,11 @@ struct Vector3 {
             x * v.y - y * v.x
         );
     }
+
     // Vector magnitude
-    float magnitude() const { return sqrt(x * x + y * y + z * z); }
+    float magnitude() const {
+        return std::sqrt(x * x + y * y + z * z);
+    }
 
     // Friend function for scalar multiplication (float * Vector3)
     friend Vector3 operator*(float scalar, const Vector3& v) {
